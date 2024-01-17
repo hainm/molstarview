b5f0f4983fd3749af7ba592a5190b2d90ea8d250
Hai Nguyen <hainm.comp@gmail.com>
2024-01-17 00:49:05 -0500

    those

--- js/lib/index.js
+++ js/lib/index.js
@@ -1,4 +1,15 @@
-// Export widget models and views, and the npm package version number.
-module.exports = require('./widget.js');
+module.exports = {};
+var loadedModules = [
+    require("./widget.js"),
+];
+for (var i in loadedModules) {
+    if (loadedModules && Object.prototype.hasOwnProperty.call(loadedModules, i)) {
+        var loadedModule = loadedModules[i];
+        for (var target_name in loadedModule) {
+            if (loadedModule && Object.prototype.hasOwnProperty.call(loadedModule, target_name)) {
+                module.exports[target_name] = loadedModule[target_name];
+            }
+        }
+    }
+}
 module.exports['version'] = require('../package.json').version;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsc0VBQXNFO0FBQ3RFLE1BQU0sQ0FBQyxPQUFPLEdBQUcsT0FBTyxDQUFDLGFBQWEsQ0FBQyxDQUFDO0FBQ3hDLE1BQU0sQ0FBQyxPQUFPLENBQUMsU0FBUyxDQUFDLEdBQUcsT0FBTyxDQUFDLGlCQUFpQixDQUFDLENBQUMsT0FBTyxDQUFDIn0=
